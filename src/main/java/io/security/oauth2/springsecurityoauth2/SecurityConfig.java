8236123842b2cab8a963897e4fae69845816f7b7
Jeidiiy <jeidiiy98@gmail.com>
2023-02-25 15:35:20 +0900

    feat: 자동설정에 의한 초기화 과정 이해

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -1,18 +1,26 @@
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin();
-        http.apply(new CustomSecurityConfigurer().setFlag(false));
+//        http.apply(new CustomSecurityConfigurer().setFlag(false));
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic();
+//        http.apply(new CustomSecurityConfigurer().setFlag(false));
         return http.build();
     }
 }
