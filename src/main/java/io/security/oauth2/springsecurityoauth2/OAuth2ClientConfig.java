c7650052d2233c34322199f6b2a9c27c3506998f
Jeidiiy <jeidiiy98@gmail.com>
2023-02-28 15:57:19 +0900

    feat: OAuth 2.0 Client Fundamentals
    application.yml, OAuth2ClientProperties, ClientRegistration, ClientRegistrationRepository 및 OAuth 2.0 Client 자동 설정 공부

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -0,0 +1,33 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class OAuth2ClientConfig {
+    @Value("${spring.security.oauth2.client.registration.keycloak.clientId}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.clientSecret}")
+    private String clientSecret;
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
+    }
+
+    private ClientRegistration keycloakClientRegistration() {
+        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
+                .registrationId("keycloak")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
+                .build();
+    }
+
+}
