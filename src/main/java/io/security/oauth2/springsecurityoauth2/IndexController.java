dea9a49e093f09da03be2139c3ee1e073bb1a73d
Jeidiiy <jeidiiy98@gmail.com>
2023-02-28 16:24:02 +0900

    fix: 오타 수정

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -6,7 +6,6 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@
 @RestController
 @RequiredArgsConstructor
 public class IndexController {
