86d277e39fbf3ac0793f2d0f86ec56006482a33e
Jeidiiy <jeidiiy98@gmail.com>
2023-03-02 19:11:30 +0900

    feat: OAuth2AuthorizedClient 활용

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/ClientController.java
@@ -0,0 +1,56 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Controller
+@RequiredArgsConstructor
+public class ClientController {
+
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
+    @GetMapping("/client")
+    public String client(HttpServletRequest request, Model model) {
+        String clientRegistrationId = "keycloak";
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        OAuth2AuthorizedClient authorizedClient1 = oAuth2AuthorizedClientRepository.loadAuthorizedClient(clientRegistrationId, authentication, request);
+        OAuth2AuthorizedClient authorizedClient2 = oAuth2AuthorizedClientService.loadAuthorizedClient(clientRegistrationId, authentication.getName());
+
+        OAuth2AccessToken accessToken = authorizedClient1.getAccessToken();
+        DefaultOAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(new OAuth2UserRequest(authorizedClient1.getClientRegistration(), accessToken));
+
+        OAuth2AuthenticationToken authenticationToken = new OAuth2AuthenticationToken(
+                oAuth2User,
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")),
+                authorizedClient1.getClientRegistration().getRegistrationId());
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        model.addAttribute("accessToken", accessToken.getTokenValue());
+        model.addAttribute("refreshToken", authorizedClient1.getRefreshToken());
+        model.addAttribute("principalName", oAuth2User.getName());
+        model.addAttribute("clientName", authorizedClient1.getClientRegistration().getClientName());
+
+        return "client";
+    }
+}
--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -2,6 +2,7 @@ package io.security.oauth2.springsecurityoauth2;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
@@ -16,11 +17,12 @@ public class OAuth2ClientConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authRequest -> authRequest.antMatchers("/home").permitAll()
+        http.authorizeRequests(authRequest -> authRequest.antMatchers("/home", "/client").permitAll()
                 .anyRequest().authenticated());
-        http.oauth2Login(authLogin -> authLogin.authorizationEndpoint(
-                authEndpoint -> authEndpoint.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())
-        ));
+//        http.oauth2Login(authLogin -> authLogin.authorizationEndpoint(
+//                authEndpoint -> authEndpoint.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())
+//        ));
+        http.oauth2Client(Customizer.withDefaults());
         http.logout().logoutSuccessUrl("/home");
         return http.build();
     }
