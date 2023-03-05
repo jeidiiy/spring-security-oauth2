d41ed1fb1274d2ba28997fc6f4141328cd6014ce
Jeidiiy <jeidiiy98@gmail.com>
2023-03-05 11:57:04 +0900

    feat: DefaultOAuth2AuthorizedClientManager - Client Credentials 권한 부여 직접 구현하기

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/LoginController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/LoginController.java
@@ -2,21 +2,12 @@ package io.security.oauth2.springsecurityoauth2.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Set;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,7 +34,6 @@ public class LoginController {
                 .attribute(HttpServletRequest.class.getName(), request)
                 .attribute(HttpServletResponse.class.getName(), response)
                 .build();
-        OAuth2AuthorizedClient oAuth2AuthorizedClient = oAuth2AuthorizedClientManager.authorize(authorizeRequest);
 
         OAuth2AuthorizationSuccessHandler successHandler = (authorizedClient, principal, attributes) -> {
             oAuth2AuthorizedClientRepository
@@ -57,24 +46,10 @@ public class LoginController {
         };
 
         oAuth2AuthorizedClientManager.setAuthorizationSuccessHandler(successHandler);
-        if (oAuth2AuthorizedClient != null) {
-            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
-            ClientRegistration clientRegistration = oAuth2AuthorizedClient.getClientRegistration();
-            OAuth2AccessToken accessToken = oAuth2AuthorizedClient.getAccessToken();
-            OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, accessToken);
-            OAuth2User oAuth2User = oAuth2UserService.loadUser(oAuth2UserRequest);
 
-            SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-            authorityMapper.setPrefix("SYSTEM_");
-            Set<GrantedAuthority> grantedAuthorities = authorityMapper.mapAuthorities(oAuth2User.getAuthorities());
+        OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientManager.authorize(authorizeRequest);
 
-            OAuth2AuthenticationToken oAuth2AuthenticationToken =
-                    new OAuth2AuthenticationToken(oAuth2User, grantedAuthorities, clientRegistration.getRegistrationId());
-
-            SecurityContextHolder.getContext().setAuthentication(oAuth2AuthenticationToken);
-
-            model.addAttribute("oAuth2AuthenticationToken", oAuth2AuthenticationToken);
-        }
+        model.addAttribute("authorizedClient", authorizedClient.getAccessToken().getTokenValue());
 
         return "home";
     }
